2c00868ed42b62eacd666aa0aa61355e4cc58870
svod011929 <80587651+svod011929@users.noreply.github.com>
2025-03-06 17:26:50 +0300

    Update install_bot.sh
    Установка зависимостей теперь происходит в виртуальном окружении

--- install_bot.sh
+++ install_bot.sh
@@ -9,8 +9,12 @@ sudo apt install -y python3 python3-pip git
 git clone https://github.com/svod011929/fenix_refer_bot.git /root/referral_bot
 cd /root/referral_bot
 
-# Установка Python-зависимостей
+# Установка Python-зависимостей в виртуальном окружении
+sudo apt install python3-venv
+python3 -m venv /root/referral_bot/venv
+source /root/referral_bot/venv/bin/activate
 pip3 install -r requirements.txt
+deactivate
 
 # Настройка systemd
 sudo cp systemd/referral_bot.service /etc/systemd/system/
